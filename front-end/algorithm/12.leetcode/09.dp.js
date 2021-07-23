456d2c7511d92db0ac1f99137e25b021ca5bafa6
cuiyangam <cuiyangam@gmail.com>
2021-07-23 17:47:57 +0800

    algo

---
+++ front-end/algorithm/12.leetcode/09.dp.js
@@ -0,0 +1,19 @@
+// 状态转移方程 斐波那契 与 count the paths 引出状态转移方程的设计方法
+// https://leetcode-cn.com/problems/longest-common-subsequence/
+
+var longestCommonSubsequence = function(text1, text2) {
+    const m = text1.length, n = text2.length;
+    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    for (let i = 1; i <= m; i++) {
+        const c1 = text1[i - 1];
+        for (let j = 1; j <= n; j++) {
+            const c2 = text2[j - 1];
+            if (c1 === c2) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+};
---
+++ front-end/algorithm/12.leetcode/10.JS语法.js
@@ -0,0 +1,66 @@
+Array slice splice shift unshift push pop
+      sort(item 被转换为字符串之后的ascII比较)
+
+const s = new Set();
+
+[2, 3, 5, 4, 5, 2, 2].forEach(x => s.add(x));
+
+for (let i of s) {
+console.log(i);
+}
+// 2 3 5 4
+
+const items = new Set([1, 2, 3, 4, 5, 5, 5, 5]);
+items.size // 5
+
+// 去除数组的重复成员
+[...new Set(array)]
+
+s.add(1).add(2).add(2);
+// 注意2被加入了两次
+
+s.size // 2
+
+s.has(1) // true
+s.has(2) // true
+s.has(3) // false
+
+s.delete(2);
+s.has(2) // false
+
+let set = new Set(['red', 'green', 'blue']);
+
+for (let item of set.keys()) {
+  console.log(item);
+}
+// red
+// green
+// blue
+
+for (let item of set.values()) {
+  console.log(item);
+}
+// red
+// green
+// blue
+
+const map = new Map([
+    ['name', '张三'],
+    ['title', 'Author']
+]);
+
+map.size // 2
+map.has('name') // true
+map.get('name') // "张三"
+
+const map = new Map([
+    ['name', '张三'],
+    ['title', 'Author']
+]);
+
+map.size // 2
+map.has('name') // true
+map.get('name') // "张三"
+
+Map: set, delete clear, set, has get, size
+Set: add, delete clear,    , has    , size
