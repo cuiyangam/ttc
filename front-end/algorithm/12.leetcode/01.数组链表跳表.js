0bd6c263605fafcecd56dd6d7569c5979808bad5
cuiyangam <cuiyangam@gmail.com>
2021-07-30 18:06:07 +0800

    二刷

--- front-end/algorithm/12.leetcode/01.数组链表跳表.js
+++ front-end/algorithm/12.leetcode/01.数组链表跳表.js
@@ -1,11 +1,38 @@
-// https://leetcode-cn.com/problems/container-with-most-water/
+/**
+知识点
+数组 高效查询 低效增删
+链表 高效增删 低效查询
+跳表 跳表是对有序链表的改进 可以给链表查询加速 空间换时间 时间复杂度到了logn
+*/
+
+// 移动零 https://leetcode-cn.com/problems/move-zeroes/ 
+// + 遍历数组的同时使用指针指向非零元素
+//     + 如果遍历到0，不做处理
+//     + 如果遍历到非0，挪到指针指向的地址
+var moveZeroes = function(nums) {
+    let pointer = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === 0) {
+            continue;
+        } else {
+            nums[pointer++] = nums[i];
+        }
+    }
+    for(let i = pointer; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+    return nums;
+};
+console.log(moveZeroes([0,1,0,3,12]));
+
+// 盛最多水的容器 https://leetcode-cn.com/problems/container-with-most-water/ 
 // + 将双指针指向容器两端，记录此时容积
 //     + 移动较短的一端，直到双指针相遇，此过程更新最大容积
 // Q：为什么该过程不会漏掉最大容积情况？
 // A: 1.导致最大面积的长短版下标必然存在
 //    2.如果移动长板，则移动后的容积一定小于移动前，相遇过程中宽减小，高度变小或者不变，面积一定变小
 //    3.所以只能移动短板，宽减小，高度可能变大，面积可能变大
-//    4.长短板相遇，我们已经沿着面积可能变大得路径遍历了所有情况，最大的面积必然已经在遍历过程中被记录
+//    4.长短板相遇，我们已经沿着面积可能变大的方向遍历了所有情况，最大的面积必然已经在遍历过程中被遍历到
 var maxArea = function(height) {
     let [l, r, max] = [0, height.length - 1, 0];
     while(l < r) {
@@ -18,28 +45,7 @@ var maxArea = function(height) {
 }
 console.log(maxArea([1,8,6,2,5,4,8,3,7]));
 
-// https://leetcode-cn.com/problems/move-zeroes/
-// + 遍历数组的同时使用指针指向非零元素
-//     + 如果遍历到0，不做处理
-//     + 如果遍历到非0，挪到指针指向的地址
-var moveZeroes = function(nums) {
-    let currIndex = 0;
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] === 0) {
-            continue;
-        } else {
-            nums[currIndex++] = nums[i];
-        }
-    }
-    for(let i = currIndex; i < nums.length; i++) {
-        nums[i] = 0;
-    }
-    return nums;
-};
-console.log(moveZeroes([0,1,0,3,12]));
-
-
-// https://leetcode-cn.com/problems/two-sum/
+// 两数之和，用例保证答案唯一 https://leetcode-cn.com/problems/two-sum/
 // + 遍历过程中找另一个加数
 //     + 如果存在，返回俩下标
 //     + 如果不存在，将加数与对应的下标存入map
@@ -54,7 +60,8 @@ var twoSum = function(nums, target) {
 };
 console.log(twoSum([2,7,11,15], 9));
 
-// https://leetcode-cn.com/problems/3sum/
+// 三数之和 https://leetcode-cn.com/problems/3sum/
+// 1.转化为两数之和,用map去重，效率不高
 var threeSum = function(nums) {
     nums.sort((a, b) => a - b);
     let result = [];
@@ -66,20 +73,14 @@ var threeSum = function(nums) {
         // 转化为两数之和
         target = 0 - nums[i];
         let map = new Map;
-
         for(let j = i + 1; j < nums.length; j++) {
             if (map.has(target - nums[j])) {
                 let second = map.get(target - nums[j]);
                 let key = `key-${nums[i]}${nums[j]}${nums[second]}`;
                 if(!resultMap.has(key)) {
-                    let item = [
-                        nums[i],
-                        nums[j],
-                        nums[second]
-                    ];
+                    let item = [ nums[i], nums[j], nums[second] ];
                     resultMap.set(key, item);
                 }
-                
             } 
             map.set(nums[j], j);
         }
@@ -88,3 +89,32 @@ var threeSum = function(nums) {
 
     return result;
 };
+console.log(threeSum([0,0,0,0]))
+
+// 2.排序 + 双指针
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const res = [];
+    for(let k = 0; k < nums.length - 2; k++){
+        if(nums[k] > 0) {
+            break;
+        }
+        if(k > 0 && nums[k] == nums[k - 1]) { // 等同前元素跳过以防止重复
+            continue;
+        }
+        let [i, j] = [k + 1, nums.length - 1];
+        while(i < j){
+            let sum = nums[k] + nums[i] + nums[j];
+            if(sum < 0){
+                while(i < j && nums[i] == nums[++i]);
+            } else if (sum > 0) {
+                while(i < j && nums[j] == nums[--j]);
+            } else {
+                res.push([nums[k], nums[i], nums[j]]);
+                while(i < j && nums[i] == nums[++i]);
+                while(i < j && nums[j] == nums[--j]);
+            }
+        }
+    }
+    return res;
+}
--- front-end/algorithm/12.leetcode/02.栈与队列.js
+++ front-end/algorithm/12.leetcode/02.栈与队列.js
@@ -1,3 +1,8 @@
+/**
+栈
+队列 双端队列 优先队列
+ */
+
 // 有效的括号
 // https://leetcode-cn.com/problems/valid-parentheses/
 
@@ -54,26 +59,31 @@ MinStack.prototype.getMin = function () {
 // 柱状图中最大的矩形
 // https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
 
-// 最大的柱子上边线必然与单根柱子得上边线平行，可用单调栈（栈底到栈顶递增）
-// 大于站定元素，入栈
-// 小于栈顶元素，一次出站直到站定重新大于栈顶，
-// 遍历完全栈，在数组两端填充0
+// 最大的柱子上边线必然与单根柱子得上边线平行,所以遍历上边线可得到最大面积
+/**
+ * 单调栈（递增）特性：栈内元素单调递增
+ * 操作规则:新元素大于栈顶元素，入栈，小于：一直出栈直到新元素大于栈顶元素
+ * 动态特性:栈内元素A出栈后，向后找第一个比其小的元素是新元素
+ *         栈内元素A出栈后，向前找第一个比其小的元素是栈顶元素
+ * 试用范围:根据其动态特性，我们可以知道可以在操作单调栈的过程中获取从某元素开始的递增区间
+ */
+// 本题技巧:找左边界
 /**
  * @param {number[]} heights
  * @return {number}
  */
 var largestRectangleArea = function (heights) {
   let ans = 0;
-  let st = [];
+  let stack = [];
   let hs = [0, ...heights, 0];
   for (let i = 0; i < hs.length; i++) {
-    while (st.length !== 0 && hs[st[st.length - 1]] > hs[i]) {
-      let cur = st.pop();
-      let left = st[st.length - 1] + 1;
+    while (stack.length !== 0 && hs[i] < hs[stack[stack.length - 1]]) {
+      let cur = stack.pop();
+      let left = stack[stack.length - 1] + 1;
       let right = i - 1;
       ans = Math.max(ans, (right - left + 1) * hs[cur]);
     }
-    st.push(i);
+    stack.push(i);
   }
   return ans;
 }
@@ -81,27 +91,28 @@ var largestRectangleArea = function (heights) {
 // 滑动窗口最大值
 // https://leetcode-cn.com/problems/sliding-window-maximum/
 // 单调双端队列（队列左到右递减），又新有大的存在，又旧有小的就删除
+/**
+1、合法性检查：队头下标如果距离 i 超过了 k ，则应该出队。
+2、单调性维护：如果新元素大于或等于队尾元素下标所对应的值，队尾元素出队，直到队为空或者队尾元素大于新元素
+3、如次遍历一遍数组，队头就是每个滑动窗口的最大值所在下标。
+ */
 
 var maxSlidingWindow = function (nums, k) {
   const n = nums.length;
-  const q = [];
-  for (let i = 0; i < k; i++) {
-    while (q.length && nums[i] >= nums[q[q.length - 1]]) {
-      q.pop();
+  const queue = [];
+  const ans = [];
+  for (let i = 0; i < n; i++) {
+    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
+      queue.pop();
     }
-    q.push(i);
-  }
+    queue.push(i);
 
-  const ans = [nums[q[0]]];
-  for (let i = k; i < n; i++) {
-    while (q.length && nums[i] >= nums[q[q.length - 1]]) {
-      q.pop();
+    while (queue[0] <= i - k) {
+      queue.shift();
     }
-    q.push(i);
-    while (q[0] <= i - k) {
-      q.shift();
+    if(i >= k - 1) {
+      ans.push(nums[queue[0]]);
     }
-    ans.push(nums[q[0]]);
   }
   return ans;
 };
