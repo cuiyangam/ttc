f686e305c88c76568d2456fd68b7a148a4e1a9f6
cuiyangam <cuiyangam@gmail.com>
2021-08-04 16:06:37 +0800

    algo

--- front-end/algorithm/12.leetcode/08.二分.js
+++ front-end/algorithm/12.leetcode/08.二分.js
@@ -1,27 +1,20 @@
 // https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
 
 var search = (nums, target) => {
-    let n = nums.length;
-    if (n == 0) {
-        return -1;
-    }
-    if (n == 1) {
-        return nums[0] == target ? 0 : -1;
-    }
-    let l = 0, r = n - 1;
+    let [l, r] = [0, nums.length - 1];
     while (l <= r) {
         let mid = Math.floor((l + r) / 2);
         if (nums[mid] == target) {
             return mid;
         }
-        if (nums[0] <= nums[mid]) { // 左边有序
-            if (nums[0] <= target && target < nums[mid]) {
+        if (nums[l] <= nums[mid]) { // 左边有序
+            if (nums[l] <= target && target < nums[mid]) {
                 r = mid - 1;
             } else {
                 l = mid + 1;
             }
         } else { // 右边有序
-            if (nums[mid] < target && target <= nums[n - 1]) {
+            if (nums[mid] < target && target <= nums[r]) {
                 l = mid + 1;
             } else {
                 r = mid - 1;
@@ -30,3 +23,61 @@ var search = (nums, target) => {
     }
     return -1;
 }
+
+// https://leetcode-cn.com/problems/binary-search/
+var search = function(nums, target) {
+    let [l, r] = [0, nums.length - 1];
+    while(l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        if(nums[mid] === target) {
+            return mid;
+        }
+        if(nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return -1;
+};
+console.log(search([-1,0,3,5,9,12], 8))
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if(target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) {
+        return false;
+    }
+    let [row, rowl, rowr] = [0, 0, matrix.length - 1];
+    while(rowl <= rowr) {
+        let mid = Math.floor((rowl + rowr) / 2);
+        if(matrix[mid][0] <= target && target <= matrix[mid][matrix[0].length - 1]) {
+            row = mid;
+            break;
+        }
+        if(target < matrix[mid][0]) {
+            rowr = mid - 1;
+        } else {
+            rowl = mid + 1;
+        }
+    }
+
+    let [columnl, columnr] = [0, 0, matrix[0].length];
+    while(columnl <= columnr) {
+        let mid = Math.floor((columnl + columnr) / 2);
+        if(target === matrix[row][mid]) {
+            return true;
+        }
+        if(target < matrix[row][mid]) {
+            columnr = mid - 1;
+        } else {
+            columnl = mid + 1;
+        }
+    }
+    return false;
+};
+
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 11));
--- front-end/algorithm/12.leetcode/09.dp.js
+++ front-end/algorithm/12.leetcode/09.dp.js
@@ -1,3 +1,30 @@
+// 动态规划是 分治 + 利用最优子结构
+// 递归是顶向下，dp是自底向上，在写dp递推方程的时候可以用递归来建立思路
+
+// https://leetcode-cn.com/problems/unique-paths-ii/
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    // 最后一行最后一列填充0方便写出dp方程
+    let dp = new Array(obstacleGrid.length + 1).fill(new Array(obstacleGrid[0].length + 1).fill(0))
+    dp[dp.length - 1][dp[0].length - 2] = 1;
+
+    for(let i = obstacleGrid.length - 1; i >= 0 ; i--) {
+        for(let j = obstacleGrid[0].length - 1; j >= 0 ; j--) {
+            if(obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+            }
+        }
+    }
+    return dp[0][0];
+};
+console.log(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+
+
 // 状态转移方程 斐波那契 与 count the paths 引出状态转移方程的设计方法
 // https://leetcode-cn.com/problems/longest-common-subsequence/
 
